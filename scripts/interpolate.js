968afc911c355d03e731defab1e4f39e44a1bf87
N Griesser <nadege_griesser@yahoo.fr>
2025-06-22 19:52:39 +0200

    Update interpolate.js

--- scripts/interpolate.js
+++ scripts/interpolate.js
@@ -5,8 +5,18 @@ const model = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/che
 async function run() {
   await model.initialize();
   const elise = mm.midiToSequenceProto(fs.readFileSync('midi/fur_elise.mid'));
-  const jazz = mm.midiToSequenceProto(fs.readFileSync('seeds/jazz_seed.mid'));
-  const [out] = await model.interpolate([elise, jazz], 3);
+  //const jazz = mm.midiToSequenceProto(fs.readFileSync('seeds/jazz_seed.mid'));
+  const jazzSeed = {
+  notes: [
+    { pitch: 60, startTime: 0, endTime: 0.5 },
+    { pitch: 63, startTime: 0.5, endTime: 1 },
+    { pitch: 67, startTime: 1, endTime: 1.5 },
+    { pitch: 70, startTime: 1.5, endTime: 2 }
+  ],
+  totalTime: 2,
+  quantizationInfo: { stepsPerQuarter: 4 }
+};
+  const [out] = await model.interpolate([elise, jazzSeed], 3);
   fs.writeFileSync('audio/combined.mid', mm.sequenceProtoToMidi(out));
 }
 run();
