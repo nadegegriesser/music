b0a5e9b4e6d1d36518a3a42c1b924a2b1a51da8e
N Griesser <nadege_griesser@yahoo.fr>
2025-06-22 19:42:52 +0200

    Update convert_audio.js

--- scripts/convert_audio.js
+++ scripts/convert_audio.js
@@ -1,3 +1,3 @@
 const { execSync } = require('child_process');
-execSync(`fluidsynth -ni soundfont.sf2 audio/combined.mid -F audio/lofi.wav -r 44100`);
+execSync(`fluidsynth -ni 'Creative (emu10k1)8MBGMSFX.SF2' audio/combined.mid -F audio/lofi.wav -r 44100`);
 execSync(`ffmpeg -i audio/lofi.wav -af "lowpass=f=1500,acompressor,volume=1.1" audio/lofi_final.wav`);
