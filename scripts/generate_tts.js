6a3048ad5d12c1aead376ad90f6f8016e03930b6
N Griesser <nadege_griesser@yahoo.fr>
2025-06-22 19:27:36 +0200

    Create generate_tts.js

---
+++ scripts/generate_tts.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const googleTTS = require('google-tts-api');
+(async()=>{
+  const url = googleTTS.getAudioUrl("Here is Für Elise in a smooth jazz style, enjoy!", {lang:'en'});
+  execSync(`curl "${url}" -o audio/speech.mp3`);
+})();
