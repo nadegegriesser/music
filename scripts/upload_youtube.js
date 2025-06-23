997e8b80a6a6c07975bff2dc4b72401d28d2ac38
N Griesser <nadege_griesser@yahoo.fr>
2025-06-23 08:50:06 +0200

    Update upload_youtube.js

--- scripts/upload_youtube.js
+++ scripts/upload_youtube.js
@@ -2,9 +2,8 @@ import {readFileSync, createReadStream} from 'fs';
 import {google} from 'googleapis';
 
 const OAuth2 = google.auth.OAuth2;
-const credentials = JSON.parse(fs.readFileSync('client_secret.json'));
-const oauth2 = new OAuth2(credentials.installed.client_id, credentials.installed.client_secret, credentials.installed.redirect_uris[0]);
-oauth2.setCredentials(JSON.parse(fs.readFileSync('token.json')));
+const oauth2 = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'https://developers.google.com/oauthplayground');
+oauth2.setCredentials({refresh_token: process.env.GOOGLE_REFRESH_TOKEN});
 const youtube = google.youtube({ version: 'v3', auth: oauth2 });
 
 async function upload() {
